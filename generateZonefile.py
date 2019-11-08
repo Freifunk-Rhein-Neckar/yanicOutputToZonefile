35c6f4848c6f22d83f5fe05f67f16ff0a08961c0
herbetom <dev@herbetom.de>
2019-11-08 14:29:00 +0100

    validate ISO 8601 of node-firstseen attribute

--- generateZonefile.py
+++ generateZonefile.py
@@ -6,6 +6,7 @@ import time
 from config import ZONE_TPL, LINE_TPL, DOMAIN, HOSTMASTERMAIL, MESHVIEWERJSON, GETWARNINGS
 
 IPv6regex = "(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"
+ISO8601regex = "^(?:[1-9]\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d(?:Z|[+-][01]\d:?[0-5]\d)$"
 
 class ffnode:
     def __init__(self, hostname, address, firstseen):
@@ -34,9 +35,12 @@ with urllib.request.urlopen(MESHVIEWERJSON) as url:
                 for address in node["addresses"]:
                     # don't use unicast addresses or the dns server ip of a gateway
                     if address[:2] != "fe" and address[-4:] !="::53" and re.match(IPv6regex,address):
-                        nodes.append(ffnode(node["hostnameLower"], address, node["firstseen"]))
-                        # print("\t\t\t\t" + node["hostnameLower"] + ": " + address)
-                        break
+                        if re.match(ISO8601regex, node["firstseen"]):
+                            nodes.append(ffnode(node["hostnameLower"], address, node["firstseen"]))
+                            # print("\t\t\t\t" + node["hostnameLower"] + ": " + address)
+                            break
+                        elif GETWARNINGS:
+                            print("false-fseen: \t" + node["hostnameLower"] + " " + node["firstseen"])
                     elif GETWARNINGS:
                         print("false-Addr: \t" + node["hostnameLower"]+" "+address)
                 else:
